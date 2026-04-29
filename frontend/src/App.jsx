49cde223eb21759859055b00270e2c8f74bfb4d6
Laila <l.takiouate@gmail.com>
2026-04-29 01:34:16 +0100

    fix navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,13 @@ import Homepage from './pages/Homepage'
 import Moviepage from './pages/Moviepage'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-import { Route, Routes } from 'react-router'
+import { Route, Routes, Navigate } from 'react-router'
 import {Toaster} from 'react-hot-toast'
 import { useAuthStore } from './store/AuthStore'
 import AIRecommendations from './pages/AIRecommendations'
 
 const App = () => {
-  const {fetchUser, fetchingUser} = useAuthStore();
+  const {fetchUser, fetchingUser, user} = useAuthStore();
 
   useEffect(() => {fetchUser()}, [fetchUser]);
   if(fetchingUser){
@@ -22,11 +22,11 @@ const App = () => {
       <Navbar />
 
       <Routes>
-        <Route path={"/"} element={<Homepage />} />
-        <Route path={"/movie/:id"} element={<Moviepage />} />
-        <Route path={"/signin"} element={<SignIn />} />
-        <Route path={"/signup"} element={<SignUp />} />
-        <Route path={"/ai-recommendations"} element={<AIRecommendations />} />
+        <Route path={"/"} element={user ? <Homepage /> : <Navigate to="/signin" />} />
+        <Route path={"/movie/:id"} element={user ? <Moviepage /> : <Navigate to="/signin" />} />
+        <Route path={"/ai-recommendations"} element={user ? <AIRecommendations /> : <Navigate to="/signin" />} />
+        <Route path={"/signin"} element={!user ? <SignIn /> : <Navigate to="/" />} />
+        <Route path={"/signup"} element={!user ? <SignUp /> : <Navigate to="/" />} />
       </Routes>
     </div>
   )
